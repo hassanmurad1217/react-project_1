622ff2c7402d3be8d82e3098c16163c2fef5a3b1
Hassan Murad <hassanmurad1217@gmail.com>
2026-06-17 18:54:24 +0500

    Contact Form

--- src/components/hooks/useState/ContactForm.jsx
+++ src/components/hooks/useState/ContactForm.jsx
@@ -1,23 +1,55 @@
+import { useState } from "react";
 import "./index.css";
 export const ContactForm = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
   return (
     <div className="container">
       <div className="card">
         <h1>Contact Form</h1>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" required autoComplete="off" />
+          <input
+            type="text"
+            name="username"
+            required
+            autoComplete="off"
+            value={user.username}
+            onChange={handleInputChange}
+          />
 
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" required autoComplete="off" />
+          <input
+            type="password"
+            name="password"
+            required
+            autoComplete="off"
+            value={user.password}
+            onChange={handleInputChange}
+          />
 
           <label htmlFor="message">Message</label>
           <textarea
-            type="password"
             name="message"
             required
             autoComplete="off"
             rows="6"
+            value={user.message}
+            onChange={handleInputChange}
           />
 
           <button type="submit">Send Message</button>
