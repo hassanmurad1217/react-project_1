1eed6b7e4c53a31ea68ab19e3bcf6c40c3eefb7a
Hassan Murad <hassanmurad1217@gmail.com>
2026-07-26 16:46:37 +0500

    ForwardRef

--- src/components/hooks/useRefs/ForwardRef.jsx
+++ src/components/hooks/useRefs/ForwardRef.jsx
@@ -1,11 +1,12 @@
-import { forwardRef, useId, useRef } from "react";
-
+import { useId, useRef } from "react";
+import "./index.css";
 export const ForwardRef = () => {
   const username = useRef(null);
   const password = useRef(null);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    console.log(username.current.value, password.current.value);
   };
 
   return (
@@ -16,13 +17,24 @@ export const ForwardRef = () => {
   );
 };
 
-const BeforeReact19Input = forwardRef((props, ref) => {
+// const BeforeReact19Input = forwardRef((props, ref) => {
+//   const id = useId();
+//   return (
+//     <>
+//       <label htmlFor={id}>{props.lable}</label>
+//       <input type="text" ref={ref} />
+//       <button>Submit</button>
+//     </>
+//   );
+// });
+
+const BeforeReact19Input = ({ lable, ref }) => {
   const id = useId();
   return (
     <>
-      <label htmlFor={id}>{props.lable}</label>
+      <label htmlFor={id}>{lable}</label>
       <input type="text" ref={ref} />
       <button>Submit</button>
     </>
   );
-});
+};
